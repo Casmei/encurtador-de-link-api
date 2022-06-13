2206a7ef203b3cc060c8fb5346f2cb0fcd103099
Tiago de Castro Lima <casmei@protonmail.com>
2022-06-13 11:24:13 -0300

    :sparkles: feat: Desacoplamento

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
-const Link = require('../models/Link');
+
+const controller = require('../controller/urlShortenerController');
 
 router.get('/:code/stats', async (req, res) => {
     let code = req.params.code;
@@ -36,18 +37,7 @@ generateCode = () => {
     return text;
 }
 
-router.post('/new', async function (req, res) {
-    const url = req.body.url;
-    const code = generateCode();
-    const resultado = await Link.create({
-        url,
-        code
-    });
-    const link_inteiro = `${process.env.DOMAIN}${resultado.code}`
-    console.log(link_inteiro)
-
-    res.render('stats', { title: 'Encurtador', link: link_inteiro, resultado: resultado });
-});
+router.post('/new', controller.newUrl);
 
 
 module.exports = router;
