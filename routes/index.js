3d065152bccf3f1598d6db22f0915a0a1f8d68aa
Tiago de Castro Lima <casmei@protonmail.com>
2022-06-13 11:56:58 -0300

    :fire: Remoção dos templates

--- routes/index.js
+++ routes/index.js
@@ -28,14 +28,7 @@ router.get('/', function (req, res, next) {
     res.render('index', { title: 'Encurtador' });
 });
 
-generateCode = () => {
-    let text = '';
-    let possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-    for (let i = 0; i < 5; i++) {
-        text += possible.charAt(Math.floor(Math.random() * possible.length));
-    }
-    return text;
-}
+
 
 router.post('/new', controller.newUrl);
 
